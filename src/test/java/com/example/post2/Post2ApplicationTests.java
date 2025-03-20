1b6793a065b21b4e91c4e90c5aa44a825f83656b
전영환 <jeon-younghwan@jeon-yeonghwan-ui-MacBookPro.local>
2025-03-20 19:20:29 +0900

    first commit

---
+++ src/test/java/com/example/post2/Post2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.post2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Post2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
